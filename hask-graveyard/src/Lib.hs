de4fadc61b9625e25f88bedd80c1948df03681d1
Evin Sellin <evinism@gmail.com>
2018-11-21 17:14:19 -0800

    minor minor changes

--- hask-graveyard/src/Lib.hs
+++ hask-graveyard/src/Lib.hs
@@ -2,5 +2,10 @@ module Lib
     ( someFunc
     ) where
 
+import qualified Data.Text as T
+
+greeting :: T.Text
+greeting = "Hello, world!"
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = putStrLn $ T.unpack greeting 
