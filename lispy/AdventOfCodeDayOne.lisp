9f0d09d956e75c8f55495197d98c4d91a75eac87
Evin Sellin <evinism@gmail.com>
2019-01-05 21:11:36 -0800

    wow this is awful but it works

--- lispy/AdventOfCodeDayOne.lisp
+++ lispy/AdventOfCodeDayOne.lisp
@@ -8,5 +8,41 @@
 (defun to-signed-ints (lines)
     (mapcar #'parse-integer lines))
 
-(format t "~A~%" (apply #'+ 
-    (to-signed-ints (read-into-lines fname))))
+(defun solve-day-one-part-one ()
+    (format t "~A~%" (apply #'+ 
+        (to-signed-ints (read-into-lines fname)))))
+
+(defun to-freqs (input-list &optional (init-value 0))
+    (reverse 
+        (reduce #'(lambda (acc cur) 
+            (cons 
+                (+ 
+                    (first acc)
+                    cur)
+                acc))
+            input-list
+            :initial-value (list init-value))))
+
+(defun get-freq-sequence (fname)
+    (to-freqs (to-signed-ints (read-into-lines fname))))
+
+;; garbage lisp because i dont know how to write it
+(defun find-first-dupe-aux (ht input-list prev)
+    (let ((head (first input-list)))
+        (if head
+            (if (gethash head ht)
+                head
+                (progn
+                    (setf (gethash head ht) t)
+                    (find-first-dupe-aux ht (rest input-list) head)))
+            (find-first-dupe-aux 
+                ht 
+                (rest (to-freqs (to-signed-ints (read-into-lines fname)) prev))
+                prev))))
+
+;; really garbage implementation because idk the better data structures yet
+(defun find-first-dupe (input-list)
+    (find-first-dupe-aux (make-hash-table) input-list 0))
+
+(defun solve-day-one-part-two ()
+    (find-first-dupe (get-freq-sequence fname)))
